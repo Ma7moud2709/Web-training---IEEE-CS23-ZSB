7f788d07b98deb4378b51c99ff0904b6f08bb386
Mahmoud Said <mahmoueddragon2004@gmail.com>
2023-03-17 14:28:17 +0200

    Delete Task -10 JS-Part-3 directory

--- Task -10 JS-Part-3/Task -1/Assignment 1.js
+++
@@ -1,5 +0,0 @@
-// Replace ? With Arithmetic Operators
-console.log(10 + 20 * (15 % 3) * 190 - 10 + 400); // 0
-// 15 % 3 --> 0 ........ -- > 0 * any thing = 0 
-console.log((10 * 20 * 15) % 3 * (190 * 10 * 400)); // 0
-//10 * 20 * 15 -->  3000 % 3 --> 0 ........ -- > 0 * any thing = 0 
--- Task -10 JS-Part-3/Task -1/Assignment 4.js
+++
@@ -1,9 +0,0 @@
-let points = 10;
-
-// Write Your Code Here
-
-console.log(points+points%7); // 13
-
-// Write Your Code Here
-
-console.log(points-(points/5)); // 8;
