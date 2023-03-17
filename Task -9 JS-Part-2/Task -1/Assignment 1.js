d81d84ef9f481b444c4534b4bf82ca51235fb745
Mahmoud Said <mahmoueddragon2004@gmail.com>
2023-03-17 03:49:12 +0200

    Add files via upload

---
+++ Task -9 JS-Part-2/Task -1/Assignment 1.js
@@ -0,0 +1,7 @@
+let Num1 = 10,Num2 = 20;
+console.log(Num1 + "" + Num2);
+console.log(typeof(Num1 + "" + Num2));
+console.log(`${Num1}${Num2}`);
+console.log(typeof(`${Num1}${Num2}`));
+console.log(Num1 + "\n" + Num2);
+console.log(`${Num2}${"\n"}${Num1}`);
